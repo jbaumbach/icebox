f8af26aabec34bb4fb22c5ec1e9a0f0e487a80d9
John Baumbach <john.j.baumbach@gmail.com>
2014-08-10 22:49:43 +0000

    tidy

--- icebox.js
+++ icebox.js
@@ -47,45 +47,43 @@ Pin.prototype.blip = function() {
 // LED1.slowBlink()
 
 Pin.prototype.slowBlink = function(times) {
-  var locals = {
-    self: this
-  };
-  
   times = times || 1;
-  var Hz = 50;
+  var Hz = 50;        // don't change this
+  var changeHz = 50;  // increase to make light smoother
   var blinkDurationSecs = 2;
-  var angle = (Math.PI * 1.5), loop = 0;
-  var changeHz = 40;
-  var changeStep = (Math.PI * blinkDurationSecs) / (changeHz * 2);  // should do full rotation in time period
-  var range = times * (changeHz * blinkDurationSecs);
   
+  var self = this;
+  var angle = (Math.PI * 1.5);
+  var loop = 0;
+  var changeStep = (Math.PI * blinkDurationSecs) / (changeHz * 2);  // should do full rotation in time period
+  var totalRange = times * (changeHz * blinkDurationSecs);
+  var brightness, pulseInterval;
   
   function pwm() {
-    var pulseTime = locals.brightness * (1000/Hz);
-    if (pulseTime >  0) {
-      digitalPulse(locals.self, 1, pulseTime);
+    var pulseTime = brightness * (1000/Hz);
+    if (pulseTime > 0) {
+      digitalPulse(self, 1, pulseTime);
     }
-    locals.interval = setTimeout(pwm, 1000/Hz);
+    pulseInterval = setTimeout(pwm, 1000/Hz);
   }
   
   function setLightLevel() {
-    locals.brightness = Math.abs((1 + Math.sin(angle)) / 2);
-    if (loop < range) {  // range
-      if ((typeof locals.interval) === "undefined") {
-        pwm(9999, 50);
+    brightness = Math.abs((1 + Math.sin(angle)) / 2);
+    if (loop < totalRange) {
+      if ((typeof pulseInterval) === "undefined") {
+        pwm();
       }
       
       angle += changeStep;
       loop++;
-      //pwm(level, 50);
-      setTimeout(setLightLevel, 1000 / changeHz); // wait 50 ms, so 20 times a sec
+      setTimeout(setLightLevel, 1000 / changeHz);
     } else {
-      clearTimeout(locals.interval);
+      clearTimeout(pulseInterval);
+      self.reset();
     }
   }
   
   setLightLevel();
-  // pwm(0.5, 50);
 };
 
 
@@ -347,6 +345,8 @@ log.log('Starting up...');
 log.log('Info: ');
 log.log(' * temp reading interval (secs): ' + readTempAndSaveMonitorIntervalSecs);
 log.log('----------------------------------------------');
+        
+
         
 -----------------------------');
         
