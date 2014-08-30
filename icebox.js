697fb3ae3f97fad862874218aac1e9b8503b8f41
John Baumbach <john.j.baumbach@gmail.com>
2014-08-30 06:12:21 +0000

    Removed storage and incremented version

--- icebox.js
+++ icebox.js
@@ -1,7 +1,7 @@
 /*
   Ice Box - Espruino
   
-  Edited: 8/22/2014 JB
+  Edited: 8/29/2014 JB
   
   Todo: Open source this guy with GPL
 
@@ -26,7 +26,7 @@
 //
 // Program global vars/constants
 //
-var programVersion = '0.3.7';
+var programVersion = '0.3.8';
 var readTempAndSaveMonitorIntervalSecs = 5;
 var minTempWhileCooling = -3.50;       // degrees celcius
 var hysteresisTolerance = 0.75;       // degrees celcius
@@ -145,97 +145,6 @@ Log.prototype.clear = function() {
 };
 
 
-//
-// Storage class - saves the temperature readings to storage
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-//
-
-var Storage = function() {
-  this.temperatureStorageFName = 'ibox-temps-[token].json';
-  this.maxReadingsPerPage = 24;
-  this.info = {
-  };
-  this.temps = [];
-};
-
-Storage.prototype.reset = function() {
-  log.log('reseting storage');
-  var result = 'ok';
-  var infoFName = this.temperatureStorageFName.replace('[token]', 'info');
-  try {
-    fs.unlink(infoFName);
-  } catch(e) {
-    // Swallow
-    result = 'err: '+  e;
-  }
-  return result;
-};
-
-Storage.prototype.save = function() {
-  if (this.info.totalItems > 0) {
-    log.log('saving storage: start');
-  
-    var infoFName = this.temperatureStorageFName.replace('[token]', 'info');
-    fs.writeFile(infoFName, JSON.stringify(this.info));
-  
-    var page = trunc((this.info.totalItems - 1) / this.maxReadingsPerPage);
-    var strData = JSON.stringify(this.temps);
-    var dataFName = this.temperatureStorageFName.replace('[token]', '' + page);
-    fs.writeFile(dataFName, strData);
-    
-    log.log('saving storage: done. page = ' + page);
-  } else {
-    log.log('not saving: no items');
-  }
-};
-
-
-Storage.prototype.readSummary = function() {
-  var infoFName = this.temperatureStorageFName.replace('[token]', 'info');
-  console.log('reading from: ' + infoFName);
-  
-  var storedData = fs.readFile(infoFName);
-  console.log('read data: ', storedData);
-  var result = JSON.parse(storedData);
-  
-  if (!result) {
-    console.log('(error) couldn\'t parse: ' + storedData);
-  }
-  return result;
-};
-
-Storage.prototype.readPage = function(pageNo) {
-  var dataFName = this.temperatureStorageFName.replace('[token]', '' + page);
-  var page = fs.readFile(dataFName);
-  var result = JSON.parse(page);
-  
-  if (!result) {
-    console.log('(error) couldn\'t parse: ' + page);
-  }
-  return result;
-};
-
-
-
-Storage.prototype.addReading = function(reading) {
-  
-  if (!clockStatus.set && !clockStatus.warned) {
-    log.log('(warning) adding readings and date is not set!');
-    clockStatus.warned = true;
-  }
-  
-  if (this.temps.length >= this.maxReadingsPerPage) {
-    this.save();
-    this.temps = [];
-  }
-  
-  this.info.totalItems = (this.info.totalItems ? this.info.totalItems + 1 : 1);
-  this.temps.push(reading);
-  log.log('added reading, have total items: ' + this.info.totalItems);
-};
-
-
-
 
 //
 // Monitoring
@@ -252,22 +161,6 @@ function stopMonitoring() {
   clearInterval(monitorInterval);
   monitorInterval = null;
   storage.save();
-}
-
-//
-// Stored monitoring data
-//
-function resetMonitoringData() {
-  return storage.reset();
-}
-
-function getMonitoringDataSummary() {
-  return storage.readSummary();
-}
-
-function getMonitoringDataPage(pageNo) {
-  var s = storage.readPage(pageNo);
-  return s;
 }
 
 
@@ -312,7 +205,6 @@ function readTempsAndSave() {
     }
   };
   console.log(reading);
-  storage.addReading(reading);
   
   GreenLED.blip();
 }
@@ -398,7 +290,6 @@ function onInit() {
 }
 
 function clearAll() {
-  storage.reset();
   log.clear();
   return 'ok';
 }
@@ -423,7 +314,6 @@ function perfTest() {
 //
 // Program variables
 //
-var storage = new Storage();
 var monitorInterval;
 var clk = new Clock(Date.now());
 var log = new Log();
