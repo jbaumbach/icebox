0c14c92e66a375eaa4767d23785886cb3d17ab6c
John Baumbach <john.j.baumbach@gmail.com>
2014-09-03 00:26:43 +0000

    support for charting external monitor

--- logparse/ibox-parse.js
+++ logparse/ibox-parse.js
@@ -13,9 +13,9 @@ var
   util = require('util')
 ;
 
-var fName = '/Users/jbaumbach/Desktop/ibox-log-2014-08-31.txt';
-var outTemplate = '/Users/jbaumbach/Desktop/chart-template.html';
-var outFName = '/Users/jbaumbach/Desktop/chart-try1.html';
+var fName = './ibox-log-2014-09-01.txt';
+var outTemplate = './chart-template.html';
+var outFName = './chart-try1.html';
 var outData = '';
 var lineCount = 0;
 var dataItems = 0;
@@ -23,19 +23,24 @@ var maxTemp = 40;
 var skip = 17;
 var skipFirst = 0;
 var maxDataItems = 600;
-var labels = [], tempsData = [];
+var labels = [], tempsData = [], tempsDataExternal = [];
 var maxTempFound, minTempFound;
 var hour;
 
 fs.readFile(fName, function (err, data) {
   if (err) throw err;
   var lines = data.toString().split('\n');
+  var heatOn = false;
   lines.forEach(function(line) {
     // console.log('got: ' + line);
     if (line) {
-      var tempMatch = line.match(/^(.*): .*temp is: (.*)$/); // [1] + '\n';
-      if (tempMatch && tempMatch[2] <= maxTemp && dataItems < maxDataItems) {
+      var tempMatch = line.match(/^(.*): internal:([0-9\-\. ]*), external:([0-9\-\. ]*), diff:([0-9\-\. ]*), msg:(.*)$/); // [1] + '\n';
+      if (tempMatch && tempMatch[2] <= maxTemp && tempMatch[3] <= maxTemp && dataItems < maxDataItems) {
         lineCount++;
+        heatOn = heatOn || (tempMatch[5].trim() === 'set heater on');
+        if (heatOn) {
+          console.log('heat is on!');
+        }
         if (lineCount % skip === 0 && lineCount >= skipFirst) {
           dataItems++;
           var time = new Date(tempMatch[1]);
@@ -44,12 +49,18 @@ fs.readFile(fName, function (err, data) {
             labels.push(thisHour);
             hour = thisHour;
           } else {
-            labels.push('');
+            if (heatOn) {
+              labels.push('HEAT');
+              heatOn = false;
+            } else {
+              labels.push('');
+            }
           }
           var foundTemp = tempMatch[2];
           maxTempFound = Math.max(maxTempFound || foundTemp, foundTemp);
           minTempFound = Math.min(minTempFound || foundTemp, foundTemp);
           tempsData.push(foundTemp);
+          tempsDataExternal.push(tempMatch[3]);
         }
       }
     }
@@ -61,8 +72,8 @@ fs.readFile(fName, function (err, data) {
     var outData = template.
       toString().
       replace('<%LABELS%>', util.inspect(labels)).
-      replace('<%DATA%>', util.inspect(tempsData));
-    
+      replace('<%DATA%>', util.inspect(tempsData)).
+      replace('<%DATA_EXTERNAL%>', util.inspect(tempsDataExternal));
     fs.writeFile(outFName, outData, function(err) {
       if (err) throw err;
       console.log('boom - done - (' + lineCount + ') ' + dataItems + ' data items, max=' + maxTempFound + ', min=' + minTempFound);
